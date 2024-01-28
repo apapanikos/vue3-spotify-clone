b14a009663e6944ab10c1e05b3c680e6c9376465
Alex Papanikos <alpapanikos@gmail.com>
2024-01-28 13:08:19 +0200

    env error fix

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,14 @@
 import NowPlayingBar from './views/NowPlayingBar.vue'
 import MainView from './views/MainView.vue'
 import LeftSideBar from './views/LeftSideBar.vue'
-import RightSideBar from './views/RightSideBar.vue'
 </script>
 
 <template>
   <div
-    class="bg-black relative min-h-full h-full w-full grid gap-2 grid-cols-[auto__1fr] grid-rows-[1fr__auto] p-4"
+    class="layout-container bg-black relative min-h-full h-full w-full grid gap-2 grid-cols-[auto__1fr] grid-rows-[1fr__auto] p-4"
   >
     <LeftSideBar />
     <NowPlayingBar />
     <MainView />
-    <!-- <RightSideBar /> -->
   </div>
 </template>
